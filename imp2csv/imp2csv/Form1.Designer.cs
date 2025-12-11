2f1e557cdfba1bb7be602c74665d9230acfc02b0
Lai <hamm.lai@kla.com>
2025-12-11 16:59:25 +0800

    add imp2csv

---
+++ imp2csv/imp2csv/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace imp2csv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.show_imp = new Sunny.UI.UIListBox();
+            this.export2csv = new Sunny.UI.UIButton();
+            this.SuspendLayout();
+            // 
+            // show_imp
+            // 
+            this.show_imp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.show_imp.FillColor = System.Drawing.Color.White;
+            this.show_imp.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.show_imp.Location = new System.Drawing.Point(4, 40);
+            this.show_imp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.show_imp.MinimumSize = new System.Drawing.Size(1, 1);
+            this.show_imp.Name = "show_imp";
+            this.show_imp.Padding = new System.Windows.Forms.Padding(2);
+            this.show_imp.ShowText = false;
+            this.show_imp.Size = new System.Drawing.Size(1113, 568);
+            this.show_imp.TabIndex = 0;
+            this.show_imp.Text = "show_imp";
+            // 
+            // export2csv
+            // 
+            this.export2csv.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.export2csv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.export2csv.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.export2csv.Location = new System.Drawing.Point(1124, 40);
+            this.export2csv.MinimumSize = new System.Drawing.Size(1, 1);
+            this.export2csv.Name = "export2csv";
+            this.export2csv.Size = new System.Drawing.Size(100, 35);
+            this.export2csv.TabIndex = 1;
+            this.export2csv.Text = "输出CSV";
+            this.export2csv.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.export2csv.Click += new System.EventHandler(this.export2csv_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1227, 613);
+            this.Controls.Add(this.export2csv);
+            this.Controls.Add(this.show_imp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ZoomScaleRect = new System.Drawing.Rectangle(19, 19, 800, 450);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunny.UI.UIListBox show_imp;
+        private Sunny.UI.UIButton export2csv;
+    }
+}
+
---
+++ imp2csv/imp2csv/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sunny.UI;
+//using InPlanLib;
+using InFlowLib;
+using System.Configuration;
+using System.IO;
+
+namespace imp2csv
+{
+    public partial class Form1 : UIForm
+    {
+        private IApplicationManager app;
+        private IJobManager jobManager;
+        private IJob my_job;
+        private string job_name;
+        private string savePath = ConfigurationManager.AppSettings["savePath"];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void initInFlow()
+        {
+            // 获取所有命令行参数
+            string[] commandLineArgs = Environment.GetCommandLineArgs();
+            if (commandLineArgs.Length <= 1)
+            {
+                UIMessageBox.ShowError("缺少job_name参数，请补充", false, true);
+                System.Environment.Exit(0);
+            }
+            job_name = commandLineArgs[1];
+            app = new ApplicationManager();
+            jobManager = app.JobManager();
+            if (app.ErrorMessage() == "OK")
+            {
+                my_job = jobManager.OpenJob(job_name);
+                if (jobManager.ErrorMessage()!="OK")
+                {
+                    UIMessageBox.ShowError("打开Job失败", false, true);
+                    System.Environment.Exit(0);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initInFlow();
+        }
+
+        private void export2csv_Click(object sender, EventArgs e)
+        {
+            string saveP = savePath + "/" + job_name + ".csv";
+            List<string> writeList = new List<string>();
+            IStackup stackup = my_job.Stackup();
+            try
+            {
+                if (stackup != null)
+                {
+                    List<IImpedanceConstraint> impedanceConstraints = stackup.ImpedanceConstraints().Cast<IImpedanceConstraint>().ToList();
+                    foreach (IImpedanceConstraint imp_item in impedanceConstraints)
+                    {
+                        ICopperLayer topLayer = imp_item.TopModelLayer();
+                        ICopperLayer botLayer = imp_item.BottomModelLayer();
+                        ICopperLayer traceLayer = imp_item.ControlledTraceLayer();
+
+                        string topLayerName = topLayer == null ? "\\" : topLayer.OdbLayerName();
+                        string botLayerName = botLayer == null ? "\\" : botLayer.OdbLayerName();
+                        string traceLayerName = traceLayer == null ? "\\" : traceLayer.OdbLayerName();
+
+                        string lineWidth = imp_item.OriginalTraceWidth(AvailableUnits.MIL).ToString("F3");
+                        string impedanceValue = imp_item.CalculationRequiredImpedance(AvailableUnits.OHMS).ToString("F0");
+                        string modeName = "";
+                        string curModeName = imp_item.ModelName().ToLower();
+                        if (curModeName.StartsWith("se"))
+                        {
+                            modeName = "single-ended";
+                            getLine(ref writeList, true, modeName, traceLayerName, topLayerName, botLayerName, lineWidth, "\\","\\", impedanceValue);
+                        }
+                        else if (curModeName.StartsWith("broadside"))
+                        {
+                            modeName = "differential";
+                            string diffSpacing = imp_item.CustomerRequiredDifferentialSpacing(AvailableUnits.MIL).ToString("F3");
+                            getLine(ref writeList, true, modeName, traceLayerName, topLayerName, botLayerName, lineWidth, diffSpacing, "\\", impedanceValue);
+                        }
+                        else if (curModeName.StartsWith("coplanar_se"))
+                        {
+                            modeName = "single-ended coplanar";
+                            string coplanarSpacing = imp_item.CustomerRequiredCoplanarSpacing(AvailableUnits.MIL).ToString("F3");
+                            getLine(ref writeList, true, modeName, traceLayerName, topLayerName, botLayerName, lineWidth, "\\",coplanarSpacing, impedanceValue);
+                        }
+                        else if (curModeName.StartsWith("diff"))
+                        {
+                            modeName = "differential";
+                            string diffSpacing = imp_item.CustomerRequiredDifferentialSpacing(AvailableUnits.MIL).ToString("F3");
+                            getLine(ref writeList, true, modeName, traceLayerName, topLayerName, botLayerName, lineWidth, diffSpacing,"\\", impedanceValue);
+                        }
+                        else if (curModeName.StartsWith("coplanar_diff"))
+                        {
+                            modeName = "differential coplanar";
+                            string diffSpacing = imp_item.CustomerRequiredDifferentialSpacing(AvailableUnits.MIL).ToString("F3");
+                            string coplanarSpacing = imp_item.CustomerRequiredCoplanarSpacing(AvailableUnits.MIL).ToString("F3");
+                            getLine(ref writeList, true, modeName, traceLayerName, topLayerName, botLayerName, lineWidth, diffSpacing,coplanarSpacing, impedanceValue);
+                        }
+                        else
+                        {
+                            modeName = "differential";
+                            string diffSpacing = imp_item.CustomerRequiredDifferentialSpacing(AvailableUnits.MIL).ToString("F3");
+                            getLine(ref writeList, true, curModeName, traceLayerName, topLayerName, botLayerName, lineWidth, diffSpacing, "\\", impedanceValue);
+                        }
+
+                        using (StreamWriter writer = new StreamWriter(saveP))
+                        {
+                            foreach (string item in writeList)
+                            {
+                                writer.WriteLine(item);
+                            }
+                        }
+                    }
+                    show_imp.Items.Add("------------输出成功, 路径=》  " + saveP + "  ------------------------");
+                }
+                else
+                {
+                    UIMessageBox.ShowError(my_job.ErrorMessage());
+                }
+            }
+            catch (Exception ex)
+            {
+                UIMessageBox.ShowError("输出失败 =>  " + ex.Message);
+
+
+            }
+
+        }
+
+        private string setLayerName(string topLayerName)
+        {
+            topLayerName = topLayerName.ToLower();
+            topLayerName = topLayerName.Replace("l", "");
+            topLayerName = int.Parse(topLayerName) < 10 ? "l0" + topLayerName : "l"+topLayerName;
+            return topLayerName;
+        }
+
+        private void getLine(ref List<string> writeList, bool v1, string modeName, string traceLayer, string topLayer, string botLayer, string lineWidth, string diffSpacing, string coplanarSpacing, string impedanceValue)
+        {
+            string line = modeName + "," + traceLayer + "," + topLayer + "," + botLayer + "," + lineWidth + "," + lineWidth + "," + diffSpacing + "," + diffSpacing + ","+coplanarSpacing+","+ impedanceValue;
+            writeList.Add(line);
+            if (v1)
+            {
+                show_imp.Items.Add(modeName + " => 阻抗层： " + traceLayer + "  上参考层: " + topLayer + "  下参考层: " + botLayer + "  线宽: " + lineWidth + "  线距: " + diffSpacing + "  共面距离: " + coplanarSpacing + "  欧姆: " + impedanceValue);
+            } else
+            {
+                show_imp.Items.Add("特别注意： =》 " + modeName + " => 阻抗层： " + traceLayer + "  上参考层: " + topLayer + "  下参考层: " + botLayer + "  线宽: " + lineWidth + "  线距: " + diffSpacing + "  欧姆: " + impedanceValue);
+            }
+        }
+    }
+}
